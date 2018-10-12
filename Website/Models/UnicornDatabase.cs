e3a13d8c7eeea8dfb2d5df4c55d0cbabe867b707
benjamin_emnett <benjamin_emnett@insideranken.org>
2018-10-12 17:09:31 -0500

    set up test database

---
+++ Website/Models/UnicornDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class UnicornDatabase : DbContext
+    {
+        public DbSet<Unicorn> Unicorns { get; set; }
+    }
+}
