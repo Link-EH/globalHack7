2e39d0a7365c83b851b3d1c378bbc347f3ba2ad0
benjamin_emnett <benjamin_emnett@insideranken.org>
2018-10-13 16:46:02 -0500

    added ethinicty and language model classes

---
+++ Website/Models/Ethnicity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    [Table("Ethnicty")]
+    public class Ethnicity
+    {
+        [Display(Name ="Ethnicity ID")]
+        public Guid EthnicityId { get; set; }
+
+        [Display(Name = "Ethnicity Name")]
+        public string EthnicityName { get; set; }
+    }
+}
---
+++ Website/Models/Language.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    [Table("Language")]
+    public class Language
+    {
+        [Display(Name ="Language Code")]
+        public string LanguageCode { get; set; }
+
+        [Display(Name ="English Name")]
+        public string EnglishName { get; set; }
+
+        [Display(Name ="Native Language")]
+        public string NativeLanguage { get; set; }
+    }
+}
