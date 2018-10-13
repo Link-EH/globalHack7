d5a8b356fe0549dcd13b779398bef63d745ae4a2
Geoffrey Gibson <Geoffrey_gibson@insideranken.org>
2018-10-13 10:20:06 -0500

    Skills tables, Jobskill Controller and Updating Views

--- Website/Models/EmploymentDatabase.cs
+++ Website/Models/EmploymentDatabase.cs
@@ -10,5 +10,8 @@ namespace Website.Models
     public class EmploymentDatabase : DbContext
     {
         public DbSet<Unicorn> Unicorns { get; set; }
+        public DbSet<SkillType> SkillsType { get; set; }
+        public DbSet<SkillCategory> SkillsCategory { get; set; }
+        public DbSet<JobSkill> JobSkills { get; set; }
     }
 }
---
+++ Website/Models/JobSkill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Website.Models
+{
+    [Table("JobSkill")]
+    public class JobSkill
+    {
+        [Display(Name = "Job Skill Id"), Key]
+        public Guid JobSkillId { get; set; }
+        [Display(Name = "Job Skill Name")]
+        public string JobSkillName { get; set; }
+        [Display(Name = "Skill Type")]
+        public string SkillType { get; set; }
+        [Display(Name = "Skill Category")]
+        public string SkillCategory { get; set; }
+    }
+}
