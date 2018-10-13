3ec6ef3e764dea8736a4e62c0beaae79be899932
a <a>
2018-10-13 10:53:37 -0500

    Added Resume schema

---
+++ Website/Models/Resume.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web;
+
+namespace Website.Models
+{
+    [Table("Resume")]
+    public class Resume
+    {
+        [Key, Display(Name = "#")]
+        public Guid ResumeId { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+    }
+}
