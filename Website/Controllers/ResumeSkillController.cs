6b1bbcfce633a8c42bffe3577dc1dece3405a354
Geoffrey Gibson <Geoffrey_gibson@insideranken.org>
2018-10-14 00:32:03 -0500

    Adding resume skills with view

---
+++ Website/Controllers/ResumeSkillController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class ResumeSkillController : Controller
+    {
+        public EmploymentDatabase db = new EmploymentDatabase();
+
+        public ActionResult Edit(Guid? id)
+        {
+
+
+            return View(db.SkillCategory.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
